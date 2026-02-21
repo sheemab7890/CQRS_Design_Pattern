5d9f6156170ff8bf0e4416d3ca0ad505e40d142d
sheemabraza <sheemabraza42@gmail.com>
2026-02-21 23:17:14 +0530

    Initializing CQRS Pattern

---
+++ src/test/java/com/sheemab/CQRS/CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheemab.CQRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
